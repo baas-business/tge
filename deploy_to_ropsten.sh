5c95428831a0c11ae186a699efddbfc4412a7158
Leonard Schellenberg <leonard.schellenberg@gmail.com>
2018-12-02 18:43:48 +0100

    deployed v0.0.6 to ropsten

--- deploy_to_ropsten.sh
+++ deploy_to_ropsten.sh
@@ -11,25 +11,29 @@ fi
 
 version=$1
 
+echo "Compiling Contracts"
 truffle compile
 
-
+echo "Generating Code"
 pushd scripts
-./copy_config.sh
+./generate_code.sh
 popd
 
 pushd interactor
 # deploy contracts
+echo "Deploying Contracts"
 go run main.go e d -version $version
 # setup BaasToken
+echo "Setting Up Baas Token"
 go run main.go e s -version $version
 popd
 
+echo "Copying configuration to web3 app and Etherlytics"
 pushd scripts
-./copy_config.sh
+./copy_config.sh $version
 popd
 
-
+echo "Committing and Pushing"
 git add -A
 git commit -a -m "deployed $version to ropsten"
 git tag -a $version -m 'ropsten deployment'
